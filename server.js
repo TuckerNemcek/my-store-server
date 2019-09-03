83164434cd6aba7483a9f44e7e77b0a7f9b26782
TuckerNemcek <tuckernemcek21@gmail.com>
2019-09-02 18:50:18 -0600

    having problems with products page

--- server.js
+++ server.js
@@ -1,9 +1,32 @@
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+//const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+const jwt = require('express-jwt')
+const jwksRsa = require('jwks-rsa')
 
 const PORT = process.env.PORT || 4000
 const app = express()
+
+// Set up Auth0 configuration
+const authConfig = {
+  domain: 'my-store-dev.auth0.com',
+  audience: 'http://localhost:4000'
+}
+
+// Define middleware that validates incoming bearer tokens
+const checkJwt = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${authConfig.domain}/.well-known/jwks.json`
+  }),
+
+  audience: authConfig.audience,
+  issuer: `https://${authConfig.domain}/`,
+  algorithm: ["RS256"]
+})
 
 // Middleware
 app.use(cors())
@@ -38,6 +61,12 @@ app.get('/api/categories', (req, res, next) => {
     .catch(error => {
       next(error)
     })
+})
+
+app.get('/api/external', checkJwt, (req, res) => {
+  res.send({
+    msg: 'Your Access Token was successfully validated!'
+  })
 })
  
 // Error handling
